5caf9139542ce1eba91d283f54e17daad96c0d2b
davidluong18 <31758412+davidluong18@users.noreply.github.com>
2018-12-12 23:59:14 -0800

    Create Professor.sql

---
+++ Professor.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `cs332a27`.`Professor` (
+  `SSN` INT NOT NULL,
+  `Name` VARCHAR(45) NULL,
+  `Street` VARCHAR(45) NULL,
+  `City` VARCHAR(45) NULL,
+  `State` VARCHAR(45) NULL,
+  `Zip` INT NULL,
+  `Area_code` INT NULL,
+  `Phone_number` INT NULL,
+  `Sex` VARCHAR(45) NULL,
+  `Title` VARCHAR(45) NULL,
+  `Salary` INT NULL,
+  PRIMARY KEY (`SSN`));
